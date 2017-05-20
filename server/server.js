4e03ba59724e500d7c2af515c0b384a9dc86e9b9
Luke Wilson <wilson.palooza@gmail.com>
2017-05-19 23:33:58 -0700

    add animate.css to code base

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(path.join(__dirname, '../client')));
+
+
+
 app.use(bodyParser.json());
 
 app.get('*', (req, res) => {
